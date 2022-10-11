8903671f410e3e7e44e79014552494f6c69488de
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-10-11 09:51:55 -0700

    removed leaguejs and changed to axios

--- server/routes/summoner.routes.js
+++ server/routes/summoner.routes.js
@@ -3,59 +3,42 @@ const router = express.Router();
 const axios = require('axios');
 
 require('dotenv').config({path: './config/.env'});
-let key = process.env.API_KEY
 
-const LeagueJS = require('leaguejs');
-const leagueJs = new LeagueJS(key)
+const key = process.env.API_KEY;
 
-const getSummonerPuuid = (summonerName) => {
-    return leagueJs.Summoner
-	.gettingByName(summonerName)
-	.then(data => {
-		'use strict';
-		return data.puuid
-	})
-	.catch(err => {
-		'use strict';
-		console.log(err);
-	});
-}
-
-const getMatchIDs = (puuid) => {
-    return leagueJs.Match
-        .gettingMatchIdsByPuuid(puuid, options={
-            count: 5
-        })
-        .then(data => {
-            return data
-        })
-        .catch(err => console.log(err.message))
+const getSummonerPuuid = (summonerName) =>{
+    return axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${key}`)
+        .then(res =>{
+            return res.data.puuid;
+        }).catch(err => console.log(err.message));
 }
 
 router.get('/:id', (req, res) =>{
-    return leagueJs.Summoner
-	.gettingByName(req.params.id)
-	.then(data => {
-		console.log(data)
-		return data
-	})
-	.catch(err => {
-		'use strict';
-		console.log(err);
-	});
+    axios.get(
+        `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.id}?api_key=${key}`
+    )
+    .then(response => {
+        return res.json(response.data)
+    })
+    .catch(error => {
+        console.log(error.message)
+    })
 })
 
 router.get('/:id/matches', async (req, res) =>{
-    const puuid = await getSummonerPuuid(req.params.id);
-    
-    const matchIDs = await getMatchIDs(puuid);
+    const puuid = await getSummonerPuuid(req.params.id)
+
+    const matchIDs = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=${key}`)
+        .then(res => {
+            return res.data;
+        }).catch(err => console.log(err.message))
 
     const matches = [];
 
     for (let i = 0; i < matchIDs.length; i++) {
-        const match = matchIDs[i]
+        let match = matchIDs[i]
         const matchData = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${match}?api_key=${key}`)
-        .then(res => res.data.info)
+        .then(res => res.data)
         .catch(err => console.log(err))
         matches.push(matchData)
     }
